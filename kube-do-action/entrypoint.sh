eb4c0935ff04368f3fcb0bdcca67518a3875481b
niqdev <niqdev@users.noreply.github.com>
2022-04-29 09:21:25 +0100

    update action

--- kube-do-action/entrypoint.sh
+++ kube-do-action/entrypoint.sh
@@ -4,19 +4,23 @@ set -euo pipefail
 
 ##############################
 
-PARAM_ACCESS_TOKEN=${1:?"Missing ACCESS_TOKEN"}
-PARAM_CONFIG_PATH=${2:?"Missing CONFIG_PATH"}
-PARAM_ENABLED=${3:?"Missing ENABLED"}
+PARAM_GITHUB_TOKEN=${1:?"Missing GITHUB_TOKEN"}
+PARAM_ACCESS_TOKEN=${2:?"Missing ACCESS_TOKEN"}
+PARAM_CONFIG_PATH=${3:?"Missing CONFIG_PATH"}
+PARAM_ENABLED=${4:?"Missing ENABLED"}
 
 ##############################
 
 echo "[+] kube-do"
+echo "[*] GITHUB_TOKEN=${PARAM_GITHUB_TOKEN}"
 echo "[*] ACCESS_TOKEN=${PARAM_ACCESS_TOKEN}"
 echo "[*] CONFIG_PATH=${PARAM_CONFIG_PATH}"
 echo "[*] ENABLED=${PARAM_ENABLED}"
 
-git status
-#git clone --branch=main --depth=1 git@github.com:hckops/actions.git /tmp/action-main
+git clone --branch=main --depth=1 "https://${GITHUB_TOKEN}@github.com/hckops/actions.git" /tmp/action-main
+
+pwd
+ls -la /tmp/action-main
 
 echo "::set-output name=status::OK"
 
