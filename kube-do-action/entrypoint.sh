332c8e501a0bbbc5151f43fdd574e3fd8e27e8c3
niqdev <niqdev@users.noreply.github.com>
2022-04-29 10:21:24 +0100

    update action

--- kube-do-action/entrypoint.sh
+++ kube-do-action/entrypoint.sh
@@ -17,12 +17,19 @@ echo "[*] ACCESS_TOKEN=${PARAM_ACCESS_TOKEN}"
 echo "[*] CONFIG_PATH=${PARAM_CONFIG_PATH}"
 echo "[*] ENABLED=${PARAM_ENABLED}"
 
-#git clone --branch=main --depth=1 "https://${PARAM_GITHUB_TOKEN}@github.com/hckops/actions.git" /tmp/action-main
+printenv
 
-curl -H "Authorization: token ${PARAM_GITHUB_TOKEN}" \
+# downloads only the last 2 commits
+curl -sSL -H "Authorization: token ${PARAM_GITHUB_TOKEN}" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/commits?per_page=2&page=1"
+
+# TODO use the second to last commit sha to download the previous config
+
+curl -sSL -H "Authorization: token ${PARAM_GITHUB_TOKEN}" \
   -H 'Accept: application/vnd.github.v3.raw' \
-  -o /tmp/README.md \
-  -L https://api.github.com/repos/hckops/actions/contents/README.md?ref=main
+  -o /tmp/${PARAM_CONFIG_PATH} \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/contents/${PARAM_CONFIG_PATH}"
 
 pwd
 ls -la /tmp
