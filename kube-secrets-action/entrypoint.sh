2cc4f09fa48d4f807eb88fdda9606b1a95b50fbe
niqdev <niqdev@users.noreply.github.com>
2022-05-27 22:45:28 +0100

    update kube-secrets-action

--- kube-secrets-action/entrypoint.sh
+++ kube-secrets-action/entrypoint.sh
@@ -6,6 +6,13 @@ set -euo pipefail
 
 PARAM_KUBECONFIG=${1:?"Missing KUBECONFIG"}
 PARAM_OPERATOR=${2:?"Missing OPERATOR"}
+# LASTPASS
+PARAM_LASTPASS_USERNAME=${3:?"Missing LASTPASS_USERNAME"}
+PARAM_LASTPASS_PASSWORD=${4:?"Missing LASTPASS_PASSWORD"}
+# ORACLE
+PARAM_ORACLE_PRIVATE_KEY=${5:?"Missing ORACLE_PRIVATE_KEY"}
+PARAM_ORACLE_FINGERPRINT=${6:?"Missing ORACLE_FINGERPRINT"}
+
 
 ##############################
 
@@ -13,18 +20,6 @@ PARAM_OPERATOR=${2:?"Missing OPERATOR"}
 # global param: <PARAM_OPERATOR>
 function init_secret {
   local NAMESPACE="kube-secrets"
-
-  # LASTPASS
-  local PARAM_LASTPASS_USERNAME=${3:?"Missing LASTPASS_USERNAME"}
-  local PARAM_LASTPASS_PASSWORD=${4:?"Missing LASTPASS_PASSWORD"}
-  echo "[-] LASTPASS_USERNAME=${PARAM_LASTPASS_USERNAME}"
-  echo "[-] LASTPASS_PASSWORD=${PARAM_LASTPASS_PASSWORD}"
-
-  # ORACLE
-  local PARAM_ORACLE_PRIVATE_KEY=${5:?"Missing ORACLE_PRIVATE_KEY"}
-  local PARAM_ORACLE_FINGERPRINT=${6:?"Missing ORACLE_FINGERPRINT"}
-  echo "[-] ORACLE_PRIVATE_KEY=${PARAM_ORACLE_PRIVATE_KEY}"
-  echo "[-] ORACLE_FINGERPRINT=${PARAM_ORACLE_FINGERPRINT}"
 
   helm template \
     --values chart/values.yaml \
@@ -41,6 +36,12 @@ function init_secret {
 echo "[+] kube-secrets"
 echo "[*] OPERATOR=${PARAM_OPERATOR}"
 echo "[*] KUBECONFIG=${PARAM_KUBECONFIG}"
+# LASTPASS
+echo "[-] LASTPASS_USERNAME=${PARAM_LASTPASS_USERNAME}"
+echo "[-] LASTPASS_PASSWORD=${PARAM_LASTPASS_PASSWORD}"
+# ORACLE
+echo "[-] ORACLE_PRIVATE_KEY=${PARAM_ORACLE_PRIVATE_KEY}"
+echo "[-] ORACLE_FINGERPRINT=${PARAM_ORACLE_FINGERPRINT}"
 
 # TODO dry run in ci: "test-kube-secrets.yml" should only print "helm template"
 init_secret
